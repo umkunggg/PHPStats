17af692f5b7cf8e7ebc76b63ba755bf000924e0a
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-11-03 17:27:40 -0400

    Now using Phing to test and package the library into a Phar.

---
+++ stub.php
@@ -0,0 +1,9 @@
+<?php
+Phar::mapPhar('PHPStats.phar');
+
+spl_autoload_register(function ($className) {
+	$classPath = 'phar://PHPStats.phar/lib/'.substr(str_replace('\\', '/', $className).'.php', 9);
+
+	@include($classPath);
+});
+__HALT_COMPILER(); ?>
