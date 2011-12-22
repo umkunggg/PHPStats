9feeeb2f61c24806cabf1d05c00e351faa564749
Michael Cordingley <Michael.Cordingley@gmail.com>
2011-12-22 11:51:05 -0500

    Created Kolmogorov files from the Binomial

---
+++ lib/ProbabilityDistribution/Kolmogorov.php
@@ -0,0 +1,235 @@
+<?php
+/**
+ * PHP Statistics Library
+ *
+ * Copyright (C) 2011-2012 Michael Cordingley<Michael.Cordingley@gmail.com>
+ * 
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Library General Public License as published
+ * by the Free Software Foundation; either version 3 of the License, or 
+ * (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Library General Public
+ * License for more details.
+ * 
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; if not, write to the Free Software Foundation, 
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 
+ * LGPL Version 3
+ *
+ * @package PHPStats
+ */
+ 
+namespace PHPStats\ProbabilityDistribution;
+
+/**
+ * Kolmogorov class
+ * 
+ * asdf
+ *
+ * For more information, see: http://en.wikipedia.org/wiki/Kolmogorov_distribution
+ */
+class Kolmogorov extends ProbabilityDistribution {
+	private $n;
+	private $p;
+	
+	/**
+	 * Constructor function
+	 *
+	 * @param float $p The probability of success in a single trial
+	 * @param int $n The number of trials
+	 */
+	public function __construct($p = 0.5, $n = 1) {
+		$this->p = $p;
+		$this->n = $n;
+	}
+	
+	/**
+	 * Returns a random variate of $n trials at $p probability each
+	 * 
+	 * @return float The random variate.
+	 * @todo Untested
+	 */
+	public function rvs() {
+		return self::getRvs($this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the probability distribution function
+	 * 
+	 * @param float $x The test value
+	 * @return float The probability
+	 */
+	public function pmf($x) {
+		return self::getPmf($x, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the cumulative distribution function, the probability of getting the test value or something below it
+	 * 
+	 * @param float $x The test value
+	 * @return float The probability
+	 */
+	public function cdf($x) {
+		return self::getCdf($x, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the survival function, the probability of getting the test value or something above it
+	 * 
+	 * @param float $x The test value
+	 * @return float The probability
+	 */
+	public function sf($x) {
+		return self::getSf($x, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the percent-point function, the inverse of the cdf
+	 * 
+	 * @param float $x The test value
+	 * @return float The value that gives a cdf of $x
+	 */
+	public function ppf($x) {
+		return self::getPpf($x, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the inverse survival function, the inverse of the sf
+	 * 
+	 * @param float $x The test value
+	 * @return float The value that gives an sf of $x
+	 */
+	public function isf($x) {
+		return self::getIsf($x, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns the moments of the distribution
+	 * 
+	 * @param string $moments Which moments to compute. m for mean, v for variance, s for skew, k for kurtosis.  Default 'mv'
+	 * @return type array A dictionary containing the first four moments of the distribution
+	 */
+	public function stats($moments = 'mv') {
+		return self::getStats($moments, $this->p, $this->n);
+	}
+	
+	/**
+	 * Returns a random variate of $n trials at $p probability each
+	 * 
+	 * @param float $p The probability of success per trial.
+	 * @param int $n The number of trials.
+	 * @return float The random variate.
+	 * @static
+	 * @todo Untested
+	 */
+	public static function getRvs($p = 0.5, $n = 1) {
+		$successes = 0;
+
+		for ($i = 0; $i < $n; $i++) {
+			if (self::BernoulliTrial($p)) $successes++;
+		}
+
+		return $successes;
+	}
+	
+	/**
+	 * Returns the probability mass function
+	 * 
+	 * @param float $x The test value
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return float The probability
+	 * @static
+	 */
+	public static function getPmf($x, $p = 0.5, $n = 1) {
+		return \PHPStats\Stats::combinations($n, $x)*pow($p, $x)*pow(1 - $p, $n - $x);
+	}
+	
+	/**
+	 * Returns the cumulative distribution function, the probability of getting the test value or something below it
+	 * 
+	 * float $x The test value
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return float The probability
+	 * @static
+	 */
+	public static function getCdf($x, $p = 0.5, $n = 1) {
+		$sum = 0.0;
+		for ($count = 0; $count <= $x; $count++) {
+			$sum += self::getPmf($count, $p, $n);
+		}
+		return $sum;
+	}
+	
+	/**
+	 * Returns the survival function, the probability of getting the test value or something above it
+	 * 
+	 * @param float $x The test value
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return float The probability
+	 * @static
+	 */
+	public static function getSf($x, $p = 0.5, $n = 1) {
+		return 1.0 - self::getCdf($x, $p, $n);
+	}
+	
+	/**
+	 * Returns the percent-point function, the inverse of the cdf
+	 * 
+	 * @param float $x The test value
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return float The value that gives a cdf of $x
+	 * @static
+	 */
+	public static function getPpf($x, $p = 0.5, $n = 1) {
+		$i = 0;
+		$cdf = 0;
+		
+		while ($cdf < $x) {
+			$cdf += self::getPmf($i, $p, $n);
+			$i++;
+		}
+		
+		return $i - 1;
+	}
+	
+	/**
+	 * Returns the inverse survival function, the inverse of the sf
+	 * 
+	 * @param float $x The test value
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return float The value that gives an sf of $x
+	 * @static
+	 */
+	public static function getIsf($x, $p = 0.5, $n = 1) {
+		return self::getPpf(1.0 - $x, $p, $n);
+	}
+	
+	/**
+	 * Returns the moments of the distribution
+	 * 
+	 * @param string $moments Which moments to compute. m for mean, v for variance, s for skew, k for kurtosis.  Default 'mv'
+	 * @param float $p The probability of success per trial
+	 * @param int $n The number of trials
+	 * @return type array A dictionary containing the first four moments of the distribution
+	 * @static
+	 */
+	public static function getStats($moments = 'mv', $p = 0.5, $n = 1) {
+		$return = array();
+
+		if (strpos($moments, 'm') !== FALSE) $return['mean'] = $n*$p;
+		if (strpos($moments, 'v') !== FALSE) $return['variance'] = $n*$p*(1-$p);
+		if (strpos($moments, 's') !== FALSE) $return['skew'] = (1-2*$p)/sqrt($n*$p*(1-$p));
+		if (strpos($moments, 'k') !== FALSE) $return['kurtosis'] = (1 - 6*$p*(1 - $p))/($n*$p*(1-$p));
+		
+		return $return;
+	}
+}
